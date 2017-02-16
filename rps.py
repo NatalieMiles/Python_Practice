79cbfd1352f342d20b2a2cbdd6769f3767c85708
Natalie <Nat.ann.miles@gmail.com>
2017-02-15 19:47:14 -0800

    Updated RPS minigame

--- rps.py
+++ rps.py
@@ -9,7 +9,8 @@ player = False
  
 while player == False:
 #set player to True
-    player = input("Choose either rock, paper, scissors? Or press 'q' to quit")
+    player = raw_input("Choose either rock, paper, " +
+                       "scissors. Press \'q\' to quit\n")
     if player == computer:
         print("Tie!, try again!")
     elif player == "rock":
@@ -27,8 +28,11 @@ while player == False:
             print("You lose. You get nothing!")
         else:
             print("You win!")
+    elif choice == "q":
+            exit()
     else:
         print("That's not an option you can play. Try again!")
-    #player was set to True, but we want it to be False so the loop continues
+
+
     player = False
     computer = player_input[randint(0,2)]
